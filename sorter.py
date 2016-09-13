d0503e999ead2ac4d31a5973a62e680130b043c1
maherb <maherb@rpi.edu>
2016-09-13 00:16:22 -0400

    Fixed errors when: date taken is unknown, file extension is in caps

--- sorter.py
+++ sorter.py
@@ -11,13 +11,17 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # takes the path of an imagine and returns the date as a string: YYYY-MM-DD
 def get_date_taken(path):
-	return Image.open(path)._getexif()[36867].split(" ")[0].replace(":","-")
+	try:
+		return Image.open(path)._getexif()[36867].split(" ")[0].replace(":","-")
+	except KeyError:
+		return "0000-00-00"
 
 # iterates through jpg images in the directory and copies them to a
 # subdirectory named YYYY-MM-DD
 for filename in os.listdir(dir_path):
-	if ".jpg" in filename or ".jpeg" in filename:
+	if ".jpg" in filename.lower() or ".jpeg" in filename.lower():
+		print filename
 		date = get_date_taken(filename)
 		if date not in os.listdir(dir_path):
 			os.mkdir(date)
-		shutil.copyfile(filename, "%s/%s" % (date, filename))
+		shutil.copyfile(filename, "%s/%s" % (date, filename))`
